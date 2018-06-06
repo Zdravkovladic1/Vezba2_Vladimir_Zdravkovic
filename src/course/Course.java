113089278c347827675ee6b188c802f8b81238f4
Vlada Zdravkovic <zdravko198727@gmail.com>
2018-06-06 12:15:23 +0200

    Prosirivanje Info u okviru klase Student

--- src/course/Course.java
+++ src/course/Course.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package course;
 
-/**
- *
- * @author qa-prepodne
- */
+
 public class Course {
+    private String name;
+    private int numberOfClasses;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfClasses() {
+        return numberOfClasses;
+    }
+
+    public void setNumberOfClasses(int numberOfClasses) {
+        this.numberOfClasses = numberOfClasses;
+    }
+    
+    
     
 }
